bc401c1cbde7340a3c8854624b59b27ecc499c29
Eric Waters <eswaters@asu.edu>
2021-11-05 14:25:49 -0700

    theming

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var http = require('http');
 var server = http.Server(app);
 
 app.use(express.static('client'));
-app.use('/', express.static(__dirname + '/www')); // redirect root
+//app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+//app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 server.listen(PORT, function() {
   console.log(`Website running on port ${PORT}`);
